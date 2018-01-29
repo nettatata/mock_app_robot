d81149ad7128387f63e080333f9582ad8b6dd31a
Weerawat Seetalalai <weerawatseetalalai@ip-172-23-250-153.ap-southeast-1.compute.internal>
2018-01-29 18:52:16 +0700

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
-Xvfb :99 -screen 0 1920x1080x24 -ac &
-export DISPLAY=:99
+#use_chrome.sh
+#Xvfb :99 -screen 0 1920x1080x24 -ac &
+#export DISPLAY=:99
 
-robot -i db -i web -i db test.robot
+use_chrome_headless.sh
+robot -v BROWSER:chrome -i db -i web -i db test.robot
+pkill chromium
+
+#pkill Xvfb                  
+
+
